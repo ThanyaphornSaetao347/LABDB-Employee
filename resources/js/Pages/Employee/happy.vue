905d54031e2d4f592944c3f02b1ce939b4c42f01
ThanyaphornSaetao347 <142294145+ThanyaphornSaetao347@users.noreply.github.com>
2024-01-08 13:15:15 +0700

    first commit

---
+++ resources/js/Pages/Employee/happy.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(department, index) in dept" :key="index">{{ department }}</li>
+        </ul>
+        <br>
+        <ul>
+            <li v-for="(dept_emp, index) in emp" :key="index">{{ dept_emp }}</li>
+        </ul>
+    </div>
+</template>
+
+<script setup> 
+const { dept , emp } = defineProps(['dept','emp']);
+</script>
